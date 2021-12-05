c2f9baa92f0e75f3be63a54560faa07bcb7f9908
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-12-05 13:29:30 -0300

    Comparator e Refatoração

---
+++ src/main/java/StreamApi/ComparatorOrdemNomeContato.java
@@ -0,0 +1,15 @@
+package StreamApi;
+
+import java.util.Comparator;
+import java.util.Map;
+
+class ComparatorOrdemNomeContato implements Comparator<Map.Entry<Integer, Contato>> {
+
+    @Override
+
+    public int compare(Map.Entry<Integer, Contato> cont1, Map.Entry<Integer, Contato> cont2) {
+
+        return cont1.getValue().getNome().compareToIgnoreCase(cont2.getValue().getNome());
+
+    }
+}
