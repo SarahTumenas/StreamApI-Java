a5afb11d89cf4a526e71868dfa32bb1147bdf0f8
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-12-06 10:24:31 -0300

    Implementando Collections e Streams com Java - Queue

---
+++ src/main/java/Queue/ExemploLinkedList.java
@@ -0,0 +1,60 @@
+package Queue;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ExemploLinkedList {
+
+    public static void main(String[] args) {
+
+        Queue<String> filaBanco = new LinkedList<>();
+
+        filaBanco.add("Patricia");
+        filaBanco.add("Roberto");
+        filaBanco.add("Flávio");
+        filaBanco.add("Pamela");
+        filaBanco.add("André");
+
+        System.out.println(filaBanco);
+
+        String clienteAserAtendido = filaBanco.poll();
+
+        System.out.println(clienteAserAtendido);
+
+        System.out.println(filaBanco);
+
+        String primeiroCliente = filaBanco.peek();
+
+        System.out.println(primeiroCliente);
+
+        System.out.println(filaBanco);
+
+        //filaBanco.clear();
+
+        String primeiroClienteOuErro = filaBanco.element();
+
+        System.out.println(primeiroClienteOuErro);
+
+        System.out.println(filaBanco);
+
+        for (String client : filaBanco){
+
+            System.out.println(client);
+
+        }
+
+        Iterator <String> iteratorFilaBanco = filaBanco.iterator();
+
+        while (iteratorFilaBanco.hasNext()){
+            System.out.println("---> " + iteratorFilaBanco.next());
+
+            }
+
+        System.out.println(filaBanco.size());
+
+        System.out.println(filaBanco.isEmpty());
+
+
+    }
+}
